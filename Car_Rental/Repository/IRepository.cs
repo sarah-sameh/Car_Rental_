dae7995093c8821abcbba51d9a959c89ab5385fe
sarah-sameh <zarasamy18@gmail.com>
2024-05-02 00:56:38 +0300

    gg

--- Car_Rental/Repository/IRepository.cs
+++ Car_Rental/Repository/IRepository.cs
@@ -2,13 +2,14 @@
 
 namespace Car_Rental.Repository
 {
-    public interface IRepository<T> where T : class,ISoftDeletable
+    public interface IRepository<T> where T : class, ISoftDeletable
     {
-         public List<T> getAll();
+        public List<T> getAll();
         public T get(int id);
         public void delete(int id);
         public void Insert(T item);
 
         public void Update(T item);
+
     }
 }
