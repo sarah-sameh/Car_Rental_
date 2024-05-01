ff06891a8810ecf0e0559c541deef391a243617b
Reem <reem.osama3052001@gmail.com>
2024-05-02 01:06:08 +0300

    startt ....

---
+++ Car_Rental/Controllers/MaintenanceController.cs
@@ -0,0 +1,42 @@
+﻿using Car_Rental.DTOs;
+using Car_Rental.Models;
+using Car_Rental.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Car_Rental.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaintenanceController : ControllerBase
+    {
+        private readonly IMaintenanceRepository maintenanceRepository;
+
+        public MaintenanceController(IMaintenanceRepository maintenanceRepository) {
+            this.maintenanceRepository = maintenanceRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<GeneralResponse> GetAll()
+        {
+            List<Maintenance> maintenances = maintenanceRepository.getAll().Where(m=>m.IsDeleted==false).ToList();
+
+            List<MaintenanceDTO> maintenanceDTOs = maintenances.Select(c => new MaintenanceDTO
+            {
+               Type = c.Type,
+               Cost = c.Cost,
+               Description = c.Description,
+               MaintenanceDate = c.MaintenanceDate,
+            }).ToList();
+
+          GeneralResponse generalResponse = new GeneralResponse()
+          {
+              IsPass = true,
+              Message=maintenanceDTOs 
+
+          };  
+            return generalResponse;
+        }
+    }
+}
---
+++ Car_Rental/DTOs/MaintenanceDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Car_Rental.DTOs
+{
+    public class MaintenanceDTO
+    {
+    
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime MaintenanceDate { get; set; }
+    }
+}
